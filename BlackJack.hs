79ebbdc1ae90974b44ae3292095609e86b761ba8
Joel Rudsberg <rudsberg@live.se>
2019-11-07 15:58:28 +0100

    labb2 first

--- BlackJack.hs
+++ BlackJack.hs
@@ -1,4 +1,13 @@
 module BlackJack where
-    import Cards
-    import RunGame
-    import Test.QuickCheck
+import Cards
+import RunGame
+import Test.QuickCheck
+
+david = Card King Hearts
+death = Card Ace Spades
+
+display :: Hand -> String
+display (Add card hand) = undefined
+
+displayCard :: Card -> String
+displayCard (Card rank suit) = show rank ++ " of " ++ show suit ++ ", "
