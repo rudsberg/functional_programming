d4a7d2852c0d01fb37cd1e910bf359baff9333b7
Joel Rudsberg <rudsberg@live.se>
2019-11-11 10:07:45 +0100

    m

--- BlackJack.hs
+++ BlackJack.hs
@@ -3,11 +3,17 @@ import Cards
 import RunGame
 import Test.QuickCheck
 
+-- Testing data
+hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
 david = Card King Hearts
 death = Card Ace Spades
 
+
+-- A1
 display :: Hand -> String
-display (Add card hand) = undefined
+display (Add card hand)
+    | size (Add card hand) == 0 = ""
+    | otherwise                 = (displayCard card) ++ (display hand)
 
 displayCard :: Card -> String
 displayCard (Card rank suit) = show rank ++ " of " ++ show suit ++ ", "
