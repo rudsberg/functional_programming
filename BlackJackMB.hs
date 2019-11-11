4e360600413b26f4f6b8077de128598f5f28038c
matteB10 <mblomq@student.chalmers.se>
2019-11-11 11:30:32 +0100

    added value functions

--- BlackJackMB.hs
+++ BlackJackMB.hs
@@ -2,10 +2,12 @@ module BlackJack where
     import Cards
     import RunGame
     import Test.QuickCheck
+    import Data.List
     
     david = Card King Hearts
     death = Card Ace Spades
     hand = Add (Card (Numeric 2) Hearts)(Add (Card Jack Spades) Empty)
+    hand2 = Add death hand
     
     
     {- | Calc size of a hand
@@ -22,37 +24,34 @@ module BlackJack where
     display :: Hand -> String
     display Empty = ""
     display (Add card hand) 
-         | size hand == 0 = displayCard card
-         | size hand == 1 = "On hand: "++(displayCard card) ++ ", " ++(display hand) 
+         | size hand == 0 = displayCard card ++ "\n"
+         | otherwise = (displayCard card) ++ ", " ++(display hand) 
     
     -- | Used for testing that display function behave as intended
     prop_display :: Hand -> Bool
     prop_display Empty = display Empty == ""
-    --prop_display hand = how to test displaystring??
-    
-    {- ERROR WITH DISPLAY
-    *BlackJack> display hand
-    "On hand: 2 of Hearts, Jack of Spades"
-    *BlackJack> hand2 = Add death hand
-    *BlackJack> display hand2
-    "On hand: Ace of Spades, On hand: 2 of Hearts, Jack of Spades"
-    *BlackJack> hand3 = Add (Card Ace Hearts) hand2
-    *BlackJack> display hand3
-    "On hand: Ace of Hearts, On hand: Ace of Spades, On hand: 2 of Hearts, Jack of Spades"
-    -}
-    
+
     
     -- | Used to display a card as a string
     displayCard :: Card -> String
     displayCard (Card (Numeric i) suit) = show i ++ " of " ++ show suit
-    displayCard (Card rank suit) = show rank ++ " of " ++ show suit
-    
+    displayCard (Card rank suit) = show rank ++ " of " ++ show suit 
+
     -- | Used for testing that display function behave as intended
     prop_displayCard :: Card -> Bool
     prop_displayCard (Card (Numeric i) suit) = displayCard (Card (Numeric i) suit) == show i ++ " of " ++ show suit
     prop_displayCard (Card rank suit) = displayCard (Card rank suit) == show rank ++ " of " ++  show suit   -- pointless test?? 
     
-    
+
+    -- | get the value of a given hand
+    value :: Hand -> Integer
+    value hand = initialValue hand - (numberOfAces hand * 10)
+  
+    -- | get the iniial value of a given hand
+    initialValue :: Hand -> Integer
+    initialValue Empty = 0
+    initialValue (Add card hand) = valueRank (rank card) + value hand 
+
     -- | get the value of given rank
     valueRank :: Rank -> Integer
     valueRank r | r == King || r == Queen || r == Jack = 10
