d909dc88ab7073d86f742a1f61592e2db8ddd577
matteB10 <mblomq@student.chalmers.se>
2019-12-10 09:50:34 +0100

    hej labb4

--- testHaskell.hs
+++ testHaskell.hs
@@ -1,4 +1,6 @@
 import Data.List
+
+{-
 sortFile :: FilePath -> IO ()
 
 sortFile filename =
@@ -6,3 +8,27 @@ sortFile filename =
      l <- lines input
      sortedString <- sort l
      writeFile filename sortedString
+-}
+
+
+data Expr = Val Int | Div Expr Expr
+
+eval :: Expr -> Maybe Int
+eval (Val i)   = Just i
+eval (Div x y) = cases (eval x) (\n -> cases (eval y) (\m -> safeDiv n m))
+
+eval' :: Expr -> Maybe Int
+eval' (Val i)   = Just i
+eval' (Div x y) = do
+                    n <- eval x
+                    m <- eval y
+                    n `safeDiv` m
+                    
+-- | Cases used as binding operator >>= 
+cases :: Maybe Int -> (Int -> Maybe Int) -> Maybe Int
+cases m f = case m of
+                Nothing -> Nothing
+                Just x -> f x 
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv m n = if n == 0 then Nothing else Just $ div m n 
