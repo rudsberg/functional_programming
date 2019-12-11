2022236a4575805df1a8a72cff26a9c71e7a21cc
Joel Rudsberg <rudsberg@live.se>
2019-12-11 12:51:54 +0100

    removed Char

--- Expr.hs
+++ Expr.hs
@@ -7,7 +7,7 @@ module Expr where
     data Expr = Num Double 
             | MonoOp MonoFunc Expr 
             | BinOp BinFunc Expr Expr
-            | Var Char
+            | Var
             deriving (Eq, Show)
 
     data MonoFunc = Sin | Cos
@@ -17,7 +17,7 @@ module Expr where
         deriving (Eq, Show)
 
     x :: Expr
-    x = Var 'x'
+    x = Var
     num :: Double -> Expr
     num d = Num d
     sin, cos :: Expr -> Expr
@@ -28,16 +28,16 @@ module Expr where
     mul exp1 exp2 = BinOp Mul exp1 exp2
 
     -- Test data -- 
-    e1 = add (mul (Num 3) (Var 'x')) (Num 17.3) -- 3*x + 17.3 
+    e1 = add (mul (Num 3) Var) (Num 17.3) -- 3*x + 17.3 
     e2 = mul (add (Num 4) (Num 3)) (add (Num 1) (Num 1))  -- (4+3)*(1+1) = 14
     e3 = mul (Num 5) (add (Num 2) (Num 3)) -- 5 * (2+3) = 25
-    e4 = mul (Var 'x') (Num 4) -- x*4
+    e4 = mul Var (Num 4) -- x*4
     ---------------
 
     -- B --
     showExpr :: Expr -> String
     showExpr (Num n) = show n 
-    showExpr (Var _)   = "x"
+    showExpr Var     = "x"
     showExpr (MonoOp Sin e) = "sin" ++ showExpr e
     showExpr (MonoOp Cos e) = "cos" ++ showExpr e
     showExpr (BinOp Add e1 e2) = showPlus e1 e2
@@ -51,7 +51,7 @@ module Expr where
     -- C --
     eval :: Expr -> Double -> Double
     eval (Num n) _ = n
-    eval (Var _) n     = n
+    eval Var n     = n
     eval (MonoOp Sin e) n = Prelude.sin $ eval e n
     eval (MonoOp Cos e) n = Prelude.cos $ eval e n
     eval (BinOp Add e1 e2) n = eval e1 n + eval e2 n
@@ -62,7 +62,7 @@ module Expr where
     expr = foldl1 add <$> chain term (char '+')
     term = foldl1 mul <$> chain factor (char '*') 
     factor = Num <$> number
-        <|> Var <$> char 'x' 
+        <|> char 'x' *> return Var
         <|> char '(' *> expr <* char ')'
 
     readExpr :: String -> Maybe Expr
@@ -75,3 +75,4 @@ module Expr where
 
     function :: Parser Expr
     function = undefined
+        
