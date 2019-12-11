96c25a85b6f9700ab7b3660dad582c6396cd33cc
matteB10 <mblomq@student.chalmers.se>
2019-12-11 12:25:40 +0100

    hej

--- Labb4MB.hs
+++ Labb4MB.hs
@@ -1,10 +1,13 @@
 import Test.QuickCheck
+import Parsing
 import Data.Maybe
+import Data.Char
 import Data.List
+import Control.Monad
 
 
 data Expr = Num Double                              
-            | Var                                   
+            | Var Char                                   
             | BinOp String (Double -> Double -> Double) Expr Expr 
             | MonoOp String (Double -> Double) Expr   
 
@@ -14,10 +17,10 @@ instance Show Expr
 
 showExpr :: Expr -> String
 showExpr (Num n)                     = show n 
-showExpr Var                         = "x"
+showExpr (Var _)                     = "x"
 showExpr (MonoOp name _ expr)        = name ++ " " ++ case expr of
                         (Num n) -> showExpr expr
-                        Var     -> showExpr expr
+                        (Var _) -> showExpr expr
                         _       -> " (" ++ showExpr expr ++ ") " 
 showExpr (BinOp name _ expr1 expr2)  = case name of
     "*" -> showFactor expr1 ++ " " ++ name ++ " " ++ showFactor expr2
@@ -29,7 +32,7 @@ showExpr (BinOp name _ expr1 expr2)  = case name of
 --------------------------------------------------------
 
 x :: Expr
-x = Var
+x = Var 'x'
 
 num :: Double -> Expr
 num d = Num d
@@ -52,17 +55,47 @@ test3 = mul (add (Num 1) (Num 2)) (add (Num 3) (Num 5)) -- = 3 * 8 = 24
 test4 = add (add (Num 1) (Num 2)) (add (Num 3) (Num 5)) -- = 3 + 8 = 11
 test5 = mul (mul (Num 1) (Num 2)) (mul (Num 3) (Num 5)) -- = 1*2 * 3*5 = 2 * 15 = 30
 
-test6 = mul (mul (Var) (Num 2)) (mul (Num 3) (Num 5)) -- = 1*2 * 3*5 = 2 * 15 = 30
+test6 = mul (mul (Var 'x') (Num 2)) (mul (Num 3) (Num 5)) -- = 1*2 * 3*5 = 2 * 15 = 30
 
 --------------------------------------------------
 
 -- | Evaluates a given expression, where the second argument is the value of x
 eval :: Expr -> Double -> Double
-eval (Var) x            = x
+eval (Var _) x          = x
 eval (Num n) _          = n
 eval (BinOp _ op i j) x = (eval i x) `op` (eval j x) 
 eval (MonoOp _ op i) x  = op (eval i x)
 
 -- | Tries to read a expression from a string
 readExpr :: String -> Maybe Expr
-readExpr = undefined
+readExpr s = undefined
+
+{-}
+            do  
+              let s' = filter (not . isSpace) s
+             
+              -}
+
+ -- | Parse a number
+number :: Parser Double
+number =  read <$> oneOrMore digit
+
+-- | Parse an x-variable
+xparse :: Parser Char
+xparse =  char 'x'
+
+expr, term, factor :: Parser Expr
+expr   = foldl1 add <$> chain term (char '+')
+term   = foldl1 mul <$> chain factor (char '*')
+factor = Num <$> number <|> (Var <$> xparse <|> char '(' *> expr <* char ')')
+
+
+
+main = do putStrLn "Welcome to the simple calculator!"
+readEvalPrint = do
+    putStr "What would you like to calculate?"
+    s <- getLine
+    let s' = filter (not . isSpace) s
+    case parse expr s' of
+       Just (e, "") -> print $ eval e 1
+       Nothing      -> putStrLn "Invalid Expression!"
